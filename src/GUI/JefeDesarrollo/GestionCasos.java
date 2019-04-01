addf17abe1452cdca338b80e9bac40639fc457d8
Alejandro <alejandroalejo714@gmail.com>
2019-03-31 18:41:50 -0600

    Solicitudes Beta

--- src/GUI/JefeDesarrollo/GestionCasos.java
+++ src/GUI/JefeDesarrollo/GestionCasos.java
@@ -11,16 +11,16 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.JOptionPane;
 import java.util.Date;
 
-public class GestionSolicitudes extends javax.swing.JFrame {
+public class GestionCasos extends javax.swing.JFrame {
     private ResultSet Data;
     private int idDepartamento;
     
-    public GestionSolicitudes() {
+    public GestionCasos() {
         initComponents();
         //txtid.setVisible(false);
         setLocationRelativeTo(null);
         txtid.setVisible(false);
-        
+        getData();
     }
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -41,18 +41,13 @@ public class GestionSolicitudes extends javax.swing.JFrame {
         jSeparator1 = new javax.swing.JSeparator();
         jLabel10 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        txtObservaciones = new javax.swing.JTextArea();
+        txtDescripcion = new javax.swing.JTextArea();
         txtid = new javax.swing.JTextField();
         jLabel11 = new javax.swing.JLabel();
         txtFecha = new javax.swing.JTextField();
-        jLabel3 = new javax.swing.JLabel();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        txtDescripcion = new javax.swing.JTextArea();
         jPanel3 = new javax.swing.JPanel();
-        btnAceptar = new javax.swing.JButton();
         btnAnterior = new javax.swing.JButton();
         btnSiguiente = new javax.swing.JButton();
-        btnRechazar = new javax.swing.JButton();
         jSeparator4 = new javax.swing.JSeparator();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -96,17 +91,17 @@ public class GestionSolicitudes extends javax.swing.JFrame {
         jPanel1.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 0, 30, 30));
 
         jLabel1.setFont(new java.awt.Font("Comic Sans MS", 1, 18)); // NOI18N
-        jLabel1.setText(".::Solicitudes::.");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 40, -1, -1));
+        jLabel1.setText(".::Casos::.");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 60, -1, -1));
 
         jPanel2.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Información Solicitud", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("SansSerif", 1, 12), new java.awt.Color(238, 112, 82))); // NOI18N
+        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Información Caso", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("SansSerif", 1, 12), new java.awt.Color(238, 112, 82))); // NOI18N
         jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel2.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(238, 112, 82));
-        jLabel2.setText("Observaciones:");
-        jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 300, -1, -1));
+        jLabel2.setText("Descripción:");
+        jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 70, -1, -1));
 
         txtNombre.setEditable(false);
         txtNombre.setBorder(null);
@@ -136,20 +131,12 @@ public class GestionSolicitudes extends javax.swing.JFrame {
         jLabel10.setText("Nombre:");
         jPanel2.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 30, -1, -1));
 
-        txtObservaciones.setColumns(20);
-        txtObservaciones.setRows(5);
-        txtObservaciones.setText("(Opcional)");
-        txtObservaciones.addFocusListener(new java.awt.event.FocusAdapter() {
-            public void focusGained(java.awt.event.FocusEvent evt) {
-                txtObservacionesFocusGained(evt);
-            }
-            public void focusLost(java.awt.event.FocusEvent evt) {
-                txtObservacionesFocusLost(evt);
-            }
-        });
-        jScrollPane1.setViewportView(txtObservaciones);
+        txtDescripcion.setEditable(false);
+        txtDescripcion.setColumns(20);
+        txtDescripcion.setRows(5);
+        jScrollPane1.setViewportView(txtDescripcion);
 
-        jPanel2.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 300, -1, -1));
+        jPanel2.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 70, -1, -1));
 
         txtid.setEnabled(false);
         jPanel2.add(txtid, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 120, 40, -1));
@@ -159,7 +146,7 @@ public class GestionSolicitudes extends javax.swing.JFrame {
         jLabel11.setText("Programador:");
         jPanel2.add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 220, -1, -1));
 
-        txtFecha.setText("YYYY-MM-DD");
+        txtFecha.setText("YYYY/MM/DD");
         txtFecha.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
                 txtFechaFocusGained(evt);
@@ -170,34 +157,12 @@ public class GestionSolicitudes extends javax.swing.JFrame {
         });
         jPanel2.add(txtFecha, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 180, 150, -1));
 
-        jLabel3.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(238, 112, 82));
-        jLabel3.setText("Descripción:");
-        jPanel2.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 70, -1, -1));
-
-        txtDescripcion.setEditable(false);
-        txtDescripcion.setColumns(20);
-        txtDescripcion.setRows(5);
-        jScrollPane2.setViewportView(txtDescripcion);
-
-        jPanel2.add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 70, -1, -1));
-
-        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 80, 370, 410));
+        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 90, 370, 310));
 
         jPanel3.setBackground(new java.awt.Color(255, 255, 255));
         jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Acciones", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("SansSerif", 1, 12), new java.awt.Color(238, 112, 82))); // NOI18N
         jPanel3.setForeground(new java.awt.Color(238, 112, 82));
         jPanel3.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        btnAceptar.setText("Aceptar");
-        btnAceptar.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        btnAceptar.setContentAreaFilled(false);
-        btnAceptar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnAceptarActionPerformed(evt);
-            }
-        });
-        jPanel3.add(btnAceptar, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 20, 60, 30));
 
         btnAnterior.setText("Anterior");
         btnAnterior.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
@@ -220,22 +185,12 @@ public class GestionSolicitudes extends javax.swing.JFrame {
         });
         jPanel3.add(btnSiguiente, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 60, 70, 30));
 
-        btnRechazar.setText("Rechazar");
-        btnRechazar.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        btnRechazar.setContentAreaFilled(false);
-        btnRechazar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnRechazarActionPerformed(evt);
-            }
-        });
-        jPanel3.add(btnRechazar, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 20, 70, 30));
-
-        jPanel1.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 500, 270, 100));
+        jPanel1.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 410, 270, 100));
 
         jSeparator4.setForeground(new java.awt.Color(0, 0, 0));
         jPanel1.add(jSeparator4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 30, 400, 20));
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 390, 610));
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 390, 540));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -253,82 +208,40 @@ public class GestionSolicitudes extends javax.swing.JFrame {
     
     private void getData(){
         btnAnterior.setEnabled(false);
-        btnSiguiente.setEnabled(true);
-       try{
+        
+       
+        try{
             Connection conn = Conexion.Conectarse();
             if(conn == null){
                throw new Exception("No se pudo Conectar");
             }
             CallableStatement proc = conn.prepareCall("{call mostrar_solicitudes (?)}");
-            proc.setInt(1, idDepartamento);
+            proc.setInt(1, WIDTH);
             this.Data = proc.executeQuery();
            if(!Data.next()){
                 JOptionPane.showMessageDialog(this,"No Hay Solicitudes pendientes de Aprobación");
                 this.dispose();
                 return;
-           }
+            }
             Data.beforeFirst();
             Data.next();
             txtid.setText(Data.getString(1));
             txtNombre.setText(Data.getString(2));
             txtDescripcion.setText(Data.getString(3));
             if(!Data.next()){
-                btnSiguiente.setEnabled(false);
+              btnSiguiente.setEnabled(false);
             }
-            Data.previous();
-            }catch(Exception e){
-                System.out.println(e.getMessage());
-                JOptionPane.showMessageDialog(null, "Fallo al recuperar la informacion");
-                this.dispose();
-            }
-    }
-    
-    private void btnAceptarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAceptarActionPerformed
-         DateFormat format = new SimpleDateFormat("YYYY-MM-DD");
-        try{ 
-            Date fecha = format.parse(txtFecha.getText());
-            System.out.println(fecha);
-        }catch(Exception e){
-            JOptionPane.showMessageDialog(this,"Formato de Fecha Invalido\n(YYYY-MM-DD)");
-            return;
-        }
-        if(cmbProgramador.getSelectedIndex() == 0){
-            JOptionPane.showMessageDialog(this,"Dese asignar un programador para el caso");
-            return;
-        }
-        if(cmbTester.getSelectedIndex() == 0){
-            JOptionPane.showMessageDialog(this,"Debe asignar un Tester al caso");
-            return;
-        }
-        try{
-            Connection conn = Conexion.Conectarse();
-            if(conn ==  null){
-                JOptionPane.showMessageDialog(this, "Ocurrio un error al conectarse");
-                return;
-            }
-            CallableStatement proc = conn.prepareCall(" { call crear_caso (?,?,?,?,?) }");
-            proc.setInt(1,Integer.parseInt(txtid.getText()));
-            proc.setString(2,txtFecha.getText());
-            proc.setInt(3,((Empleado) cmbProgramador.getSelectedItem()).getId() );
-            proc.setInt(4,((Empleado)cmbTester.getSelectedItem()).getId());
-            if(txtObservaciones.getText().equals("(Opcional)")){
-                proc.setString(5,"");
-            }else{
-                proc.setString(5,txtObservaciones.getText());
-            }
-            ResultSet answer = proc.executeQuery();
-            JOptionPane.showMessageDialog(this,answer.getString(1));
-            getData();
+             Data.previous();
         }catch(Exception e){
             System.out.println(e.getMessage());
-            JOptionPane.showMessageDialog(this, e.getMessage());
-        }     
-    }//GEN-LAST:event_btnAceptarActionPerformed
-
+            JOptionPane.showMessageDialog(null, "Fallo al recuperar la informacion");
+            this.dispose();
+        }
+    }
+    
     private void btnAnteriorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAnteriorActionPerformed
         try{
             btnSiguiente.setEnabled(true);
-            txtObservaciones.setText("(Opcional)");
             if(Data.previous()){
                 txtid.setText(Data.getString(1));
                 txtNombre.setText(Data.getString(2));
@@ -340,6 +253,7 @@ public class GestionSolicitudes extends javax.swing.JFrame {
                     btnAnterior.setEnabled(false);
                 }
                 Data.next();
+                
             }
         }catch(Exception e){
             System.out.println(e.getMessage());
@@ -352,7 +266,10 @@ public class GestionSolicitudes extends javax.swing.JFrame {
     }//GEN-LAST:event_jLabel8MouseClicked
 
     private void jLabel7MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel7MouseClicked
-        this.dispose();    
+        // TODO add your handling code here:
+        JefeDesarrolloMain a = new JefeDesarrolloMain();
+        a.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jLabel7MouseClicked
 
     private void jLabel9MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel9MouseClicked
@@ -368,7 +285,6 @@ public class GestionSolicitudes extends javax.swing.JFrame {
     private void btnSiguienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSiguienteActionPerformed
             try{
             btnAnterior.setEnabled(true);
-            txtObservaciones.setText("(Opcional)");
             if(Data.next()){
                 txtid.setText(Data.getString(1));
                 txtNombre.setText(Data.getString(2));
@@ -376,7 +292,7 @@ public class GestionSolicitudes extends javax.swing.JFrame {
                 txtFecha.setText("YYYY-MM-DD");
                 cmbTester.setSelectedIndex(0);
                 cmbProgramador.setSelectedIndex(0);
-                if(!Data.next()){
+                 if(!Data.next()){
                     btnSiguiente.setEnabled(false);
                 }
                 Data.previous();
@@ -386,14 +302,9 @@ public class GestionSolicitudes extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_btnSiguienteActionPerformed
 
-    private void btnRechazarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRechazarActionPerformed
-        JOptionPane.showInputDialog(this,"Dinos el Motivo");
-    }//GEN-LAST:event_btnRechazarActionPerformed
-
     private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
         getProgramadores();
         getTesters();
-        getData();
     }//GEN-LAST:event_formWindowOpened
 
     private void txtFechaFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtFechaFocusLost
@@ -407,18 +318,6 @@ public class GestionSolicitudes extends javax.swing.JFrame {
            txtFecha.setText("");
        }
     }//GEN-LAST:event_txtFechaFocusGained
-
-    private void txtObservacionesFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtObservacionesFocusGained
-        if(txtObservaciones.getText().equals("(Opcional)")){
-            txtObservaciones.setText("");
-        }
-    }//GEN-LAST:event_txtObservacionesFocusGained
-
-    private void txtObservacionesFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtObservacionesFocusLost
-        if(txtObservaciones.getText().equals("")){
-            txtObservaciones.setText("(Opcional)");
-        }
-    }//GEN-LAST:event_txtObservacionesFocusLost
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -499,9 +398,7 @@ public class GestionSolicitudes extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnAceptar;
     private javax.swing.JButton btnAnterior;
-    private javax.swing.JButton btnRechazar;
     private javax.swing.JButton btnSiguiente;
     private javax.swing.JComboBox<String> cmbProgramador;
     private javax.swing.JComboBox<String> cmbTester;
@@ -509,7 +406,6 @@ public class GestionSolicitudes extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
@@ -519,13 +415,11 @@ public class GestionSolicitudes extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSeparator jSeparator4;
     private javax.swing.JTextArea txtDescripcion;
     private javax.swing.JTextField txtFecha;
     private javax.swing.JTextField txtNombre;
-    private javax.swing.JTextArea txtObservaciones;
     private javax.swing.JTextField txtid;
     // End of variables declaration//GEN-END:variables
 }
